0329c830b6c98551d2533cad22ff7c27b3a4a7f4
David Wheatley <hi@davwheat.dev>
2021-02-27 20:27:09 +0000

    Import Less and fonts in gatsby-browser instead of Layout

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,7 @@
  */
 
 import 'normalize.css'
+
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import '../css/baseline.less'
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/700.css'
-import '../css/baseline.less'
-
 import ErrorBoundary from './ErrorBoundary'
 import { ThemeProvider } from '@material-ui/core'
 import theme from '../theme'
